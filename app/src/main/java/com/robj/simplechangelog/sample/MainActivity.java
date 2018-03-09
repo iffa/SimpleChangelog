42cbd0847dc5a07e6e0792a3559fbae0cd3acd60
Santeri Elo <me@santeri.xyz>
2018-03-09 23:27:43 +0200

    Added HTML stylized line to sample app

--- app/src/main/java/com/robj/simplechangelog/sample/MainActivity.java
+++ app/src/main/java/com/robj/simplechangelog/sample/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.StyleRes;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
 import android.view.View;
 
 import com.robj.simplechangelog.ui.ChangelogUtil;
@@ -35,12 +36,12 @@ public class MainActivity extends AppCompatActivity {
     private void showChangelog(@StyleRes int styleRes) {
         ChangelogBuilder builder = new ChangelogBuilder()
                 .setVersionCode(BuildConfig.VERSION_CODE)
-                .setTitle(BuildConfig.VERSION_NAME);
-
-        builder.addLineItem(getString(R.string.cl_line1));
-        builder.addMinSdkVersionLineItem(Build.VERSION_CODES.O, getString(R.string.cl_oreo));
-        builder.addMaxSdkVersionLineItem(Build.VERSION_CODES.N, getString(R.string.cl_nougat));
-        builder.addSdkVersionRangeLineItem(Build.VERSION_CODES.O, Build.VERSION_CODES.O_MR1, getString(R.string.cl_sdk_range));
+                .setTitle(BuildConfig.VERSION_NAME)
+                .addLineItem(getString(R.string.cl_line1))
+                .addLineItem(Html.fromHtml(getString(R.string.cl_line2)))
+                .addMinSdkVersionLineItem(Build.VERSION_CODES.O, getString(R.string.cl_oreo))
+                .addMaxSdkVersionLineItem(Build.VERSION_CODES.N, getString(R.string.cl_nougat))
+                .addSdkVersionRangeLineItem(Build.VERSION_CODES.O, Build.VERSION_CODES.O_MR1, getString(R.string.cl_sdk_range));
 
         ChangelogUtil.showChangelog(this, builder.build(), styleRes);
     }
